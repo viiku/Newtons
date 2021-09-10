aee88d4cbbbafcb9e45c48dc96486eedea3f558a
Vivek Kumar Singh <vivekvikkusingh@gmail.com>
2021-09-10 21:14:20 +0530

    Create Season Of Love.java

---
+++ Season Of Love.java
@@ -0,0 +1,19 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+		Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+//System.out.print("Enter first number- ");  
+        int a= sc.nextInt();  
+		int k=a%3;
+		if(k==2){
+			System.out.print((a/3)+1);
+		}else{
+			System.out.print(a/3);
+		}
+	}
+}
